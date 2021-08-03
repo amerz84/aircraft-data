e58b77df685f12880dfe49fd3547c8b4db39bdec
Amerz84 <snuesen@gmail.com>
2021-08-02 19:11:20 -0500

    Completed v1 of chart data

--- src/app/views/chart/chart.component.ts
+++ src/app/views/chart/chart.component.ts
@@ -1,8 +1,9 @@
-import { DateTimeConversion } from './../../utils/datetime-conversions';
-import { DataSharingService } from 'src/app/services/data-sharing.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChartDataSets } from 'chart.js';
-import { Color, Label } from 'ng2-charts';
+import { BaseChartDirective, Color, Label } from 'ng2-charts';
+import { DataSharingService } from 'src/app/services/data-sharing.service';
+
+import { DateTimeConversion } from './../../utils/datetime-conversions';
 
 @Component({
   selector: 'chart-view',
@@ -10,11 +11,12 @@ import { Color, Label } from 'ng2-charts';
   styleUrls: ['./chart.component.css']
 })
 export class ChartComponent implements OnInit {
+  @ViewChild(BaseChartDirective) chtChart: BaseChartDirective;
   chtDataset: any[];
   lineChartData: ChartDataSets[];
   isDataAvailable: boolean = false;
 
-  lineChartLabels = [];
+  lineChartLabels: Label[] = [];
 
   lineChartOptions = {
     responsive: true,
@@ -32,12 +34,6 @@ export class ChartComponent implements OnInit {
   lineChartType = 'line';
   
   constructor(private sharingService: DataSharingService, private converter: DateTimeConversion) {
-
-    for (let i = 0; i < this.sharingService.getCHTVals()[0].length; i++) {
-      this.lineChartLabels.push(this.converter.toSeconds(i + 1));
-      console.log(this.lineChartLabels[i]);
-    }
-
     this.lineChartData = [
       { data: this.sharingService.getCHTVals()[0], label: 'CHT 1' },
       { data: this.sharingService.getCHTVals()[1], label: 'CHT 2' },
@@ -46,11 +42,31 @@ export class ChartComponent implements OnInit {
       { data: this.sharingService.getCHTVals()[4], label: 'CHT 5' },
       { data: this.sharingService.getCHTVals()[5], label: 'CHT 6' },
     ];
+    this.lineChartLabels.length = 0;
+    this.lineChartLabels.push(...this.getLabels());
 
-    this.isDataAvailable = true;
+    setTimeout(() => {
+      if (this.chtChart && this.chtChart.chart && this.chtChart.chart.config) {
+        this.chtChart.chart.config.data.labels = this.lineChartLabels;
+        this.chtChart.chart.update();
+      }
+    }, 50);
   }
 
-  ngOnInit(): void {    
+  ngOnInit(): void {   
+
+
+
+
+    this.isDataAvailable = true; 
+  }
+
+  getLabels() {
+    const labels: string[] = [];
+    for (let i = 0; i < this.sharingService.getCHTVals()[0].length; i++) {
+      labels.push(this.converter.toSeconds(i + 1));
+    }
+    return labels;
   }
 
 }
